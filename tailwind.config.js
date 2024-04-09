63c833c7b67c241437350520fd9721444e9626dc
YOLO-GIT <ahmadsufi345@gmail.com>
2024-04-10 01:29:06 +0800

    Soem JS Patches

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}", "./**/*.{html,js,php}"],
+  content: {
+    relative: true,
+    transform: (content) => content.replace(/taos:/g, ''),
+    files: ["./*.{html,js}", "./**/*.{html,js,php}"],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('taos/plugin')
+  ],
+  safelist: [
+    '!duration-[0ms]',
+    '!delay-[0ms]',
+    'html.js :where([class*="taos:"]:not(.taos-init))'
+  ],
 }
-
