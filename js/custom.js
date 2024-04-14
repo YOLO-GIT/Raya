8e55eac9fc8312a1e0b09315f4e2ddb6bf19e1c5
YOLO-GIT <ahmadsufi345@gmail.com>
2024-04-14 12:57:17 +0800

    Some Modification

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
+const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
 const enhance = id => {
     const element = document.getElementById(id),
         text = element.innerText.split("");
@@ -32,3 +34,14 @@ const enhance = id => {
 }
 
 enhance("channel-link");
+
+let bg = document.getElementById('bg');
+let shadow_left = document.getElementById('ayat');
+
+window.addEventListener('scroll', () => {
+    let value = window.scrollY;
+
+    // Adjust the parallax effect for each image
+    // bg.style.bottom = value * 0.5 + 'px'; // Adjust the multiplier as needed
+    ayat.style.marginTop = value * 0.3 + 'px'; // Adjust the multiplier as needed
+});
