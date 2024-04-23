74fa09c496498dee95807f6732fa5ad68c97c9b8
YOLO-GIT <ahmadsufi345@gmail.com>
2024-04-24 02:10:07 +0800

    Added Some Script

--- js/custom.js
+++ js/custom.js
@@ -54,4 +54,8 @@ window.addEventListener('scroll', () => {
 
 document.getElementById("toggleButton").addEventListener("click", function () {
     document.getElementById("secretTwo").classList.toggle("hidden");
+
+    if (!secretTwo.classList.contains('hidden')) {
+        // secretTwo.classList.add('animate-fadeInDown');
+    }
 });
