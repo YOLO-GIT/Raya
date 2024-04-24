f2a1d2520cceccabeba2817a3522eaac3fd3c915
YOLO-GIT <ahmadsufi345@gmail.com>
2024-04-24 10:35:01 +0800

    Some Script Adjustment

--- js/custom.js
+++ js/custom.js
@@ -35,23 +35,6 @@ const enhance = id => {
 
 enhance("channel-link");
 
-let bg = document.getElementById('bg');
-let ayat = document.getElementById('ayat');
-let ketupat = document.getElementById('ketupat');
-let moon = document.getElementById('moon');
-let mosque = document.getElementById('mosque');
-
-window.addEventListener('scroll', () => {
-    let value = window.scrollY;
-
-    // Adjust the parallax effect for each image
-    // bg.style.bottom = value * 0.5 + 'px'; // Adjust the multiplier as needed
-    ayat.style.marginTop = value * 0.3 + 'px'; // Adjust the multiplier as needed
-    ketupat.style.marginTop = value * -0.3 + 'px'; // Adjust the multiplier as needed
-    moon.style.marginTop = value * -0.3 + 'px'; // Adjust the multiplier as needed
-    mosque.style.marginTop = value * 0.3 + 'px'; // Adjust the multiplier as needed
-});
-
 document.getElementById("toggleButton").addEventListener("click", function () {
     document.getElementById("secretTwo").classList.toggle("hidden");
 
