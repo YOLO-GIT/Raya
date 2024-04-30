a4fa0e5f43aec9cf85c940e6d8130fc40a1b476d
YOLO-GIT <ahmadsufi345@gmail.com>
2024-04-30 10:38:11 +0800

    Final Adjustment

--- js/main.js
+++ js/main.js
@@ -4,35 +4,32 @@ import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module
 import {
     OrbitControls
 } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-// To allow for importing the .gltf file
 import {
     GLTFLoader
 } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
-//Create a Three.JS Scene
-const scene = new THREE.Scene();
-//create a new camera with positions and angles
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-//Keep track of the mouse position, so we can make the ketupat move
-let mouseX = window.innerWidth / 2;
-let mouseY = window.innerHeight / 2;
-
-//Keep the 3D object on a global variable so we can access it later
 let object;
 
-//OrbitControls allow the camera to move around the scene
-let controls;
+const scene = new THREE.Scene();
 
-//Set which object to render
-let objToRender = 'second_ketupat';
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg')
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+const model_one = new THREE.TextureLoader().load('');
 
 //Instantiate a loader for the .gltf file
 const loader = new GLTFLoader();
 
 //Load the file
 loader.load(
-    `models/${objToRender}/Ketupat.gltf`,
+    `../../models/ketupat/scene.gltf`,
     function (gltf) {
         //If the file is loaded, add it to the scene
         object = gltf.scene;
@@ -41,7 +38,7 @@ loader.load(
     function (xhr) {
         //While it is loading, log the progress
         console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        console.log("Three JS Woi :D");
+        console.log("Oh no! I am being compromised :(");
     },
     function (error) {
         //If there is an error, log it
@@ -49,53 +46,29 @@ loader.load(
     }
 );
 
-//Instantiate a new renderer and set its size
-const renderer = new THREE.WebGLRenderer({
-    alpha: true
-}); //Alpha: true allows for the transparent background
-renderer.setSize(window.innerWidth, window.innerHeight);
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+const ambientLight = new THREE.AmbientLight(0xffffff);
 
-//Add the renderer to the DOM
-document.getElementById("container3D").appendChild(renderer.domElement);
+scene.add(pointLight, ambientLight);
 
-//Set how far the camera will be from the 3D model
-camera.position.z = objToRender === "dino" ? 8 : 80;
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
 
-//Add lights to the scene, so we can actually see the 3D model
-const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
-topLight.position.set(500, 500, 500) //top-left-ish
-topLight.castShadow = true;
-scene.add(topLight);
+scene.add(lightHelper, gridHelper);
 
-const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
-scene.add(ambientLight);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-//Render the scene
 function animate() {
     requestAnimationFrame(animate);
-    //Here we could add some code to update the scene, adding some automatic movement
 
-    //Make the ketupat move
-    if (object && objToRender === "second_ketupat") {
-        //I've played with the constants here until it looked good 
-        object.rotation.y = -3 + mouseX / window.innerWidth * 3;
-        object.rotation.x = -1.2 + mouseY * 2.5 / window.innerHeight;
-    }
+    if (object) object.rotation.x = 0;
+    if (object) object.rotation.y -= 0.005;
+    if (object) object.rotation.z = 0;
+
+    controls.update();
+
     renderer.render(scene, camera);
 }
 
-//Add a listener to the window, so we can resize the window and the camera
-window.addEventListener("resize", function () {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
-
-//add mouse position listener, so we can make the ketupat move
-document.onmousemove = (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
-}
-
-//Start the 3D rendering
 animate();
