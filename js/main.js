5dd447c0e85ec1f3a765762238e0e4b2baa82f0b
YOLO-GIT <ahmadsufi345@gmail.com>
2024-04-30 11:11:35 +0800

    Final Adjustment

--- js/main.js
+++ js/main.js
@@ -4,32 +4,31 @@ import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module
 import {
     OrbitControls
 } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+// To allow for importing the .gltf file
 import {
     GLTFLoader
 } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
-let object;
-
+//Create a Three.JS Scene
 const scene = new THREE.Scene();
-
+//create a new camera with positions and angles
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#bg')
-});
+//Keep the 3D object on a global variable so we can access it later
+let object;
 
-renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30);
+//OrbitControls allow the camera to move around the scene
+let controls;
 
-const model_one = new THREE.TextureLoader().load('');
+//Set which object to render
+let objToRender = 'second_ketupat';
 
 //Instantiate a loader for the .gltf file
 const loader = new GLTFLoader();
 
 //Load the file
 loader.load(
-    `../../models/ketupat/scene.gltf`,
+    `models/${objToRender}/Ketupat.gltf`,
     function (gltf) {
         //If the file is loaded, add it to the scene
         object = gltf.scene;
@@ -38,7 +37,7 @@ loader.load(
     function (xhr) {
         //While it is loading, log the progress
         console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-        console.log("Oh no! I am being compromised :(");
+        console.log("Three JS Woi :D");
     },
     function (error) {
         //If there is an error, log it
@@ -46,29 +45,40 @@ loader.load(
     }
 );
 
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5);
-const ambientLight = new THREE.AmbientLight(0xffffff);
+//Instantiate a new renderer and set its size
+const renderer = new THREE.WebGLRenderer({
+    alpha: true
+}); //Alpha: true allows for the transparent background
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-scene.add(pointLight, ambientLight);
+//Add the renderer to the DOM
+document.getElementById("container3D").appendChild(renderer.domElement);
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
+//Set how far the camera will be from the 3D model
+camera.position.z = objToRender === "dino" ? 8 : 80;
 
-scene.add(lightHelper, gridHelper);
+//Add lights to the scene, so we can actually see the 3D model
+const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
+topLight.position.set(500, 500, 500) //top-left-ish
+topLight.castShadow = true;
+scene.add(topLight);
 
-const controls = new OrbitControls(camera, renderer.domElement);
+const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "dino" ? 5 : 1);
+scene.add(ambientLight);
 
+controls = new OrbitControls(camera, renderer.domElement);
+//Render the scene
 function animate() {
     requestAnimationFrame(animate);
 
+    //Here we could add some code to update the scene, adding some automatic movement
     if (object) object.rotation.x = 0;
     if (object) object.rotation.y -= 0.005;
     if (object) object.rotation.z = 0;
 
     controls.update();
-
     renderer.render(scene, camera);
 }
 
+//Start the 3D rendering
 animate();
